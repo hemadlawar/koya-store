172bc91947836f051907fefac4f9fe53336daf96
hemaddlawar <hema.200219627512@gmail.com>
2023-09-21 02:26:08 +0300

    %99.999999999 done

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,13 +8,19 @@ export default function Footer() {
           koya shop &copy; {new Date().getFullYear()}
         </p>
       </div>
-      <div className="">
-        <div>
+      <div className="flex justify-center mb-2">
+        <p>developed by: Hema DO</p>
+      </div>
+      <div className=" flex justify-center ">
+        <a href="https://github.com/hemadlawar">
           <AiFillGithub />
-        </div>
-        <div>
-          <AiFillLinkedin />
-        </div>
+        </a>
+        <a
+          className="ml-2"
+          href="https://www.linkedin.com/in/hema-dlawar-020218292/"
+        >
+          <AiFillLinkedin className="" />
+        </a>
       </div>
     </footer>
   );
