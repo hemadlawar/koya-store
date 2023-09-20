1edead600985cdb2e3a6cba3a4869d5fc6e95780
hemaddlawar <hema.200219627512@gmail.com>
2023-09-21 02:16:20 +0300

    add icon to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 export default function Footer() {
   return (
     <footer className="bg-blue-500 text-white py-4">
@@ -7,6 +7,14 @@ export default function Footer() {
         <p className="text-sm sm:text-base">
           koya shop &copy; {new Date().getFullYear()}
         </p>
+      </div>
+      <div className="">
+        <div>
+          <AiFillGithub />
+        </div>
+        <div>
+          <AiFillLinkedin />
+        </div>
       </div>
     </footer>
   );
